bf93796f4e68933989932103125d4dcecb38c50e
Eric Martins <ericmartis123@gmail.com>
2025-02-28 18:40:59 -0300

    estrutura base do banco criada

--- painel/filtros/criar.php
+++ painel/filtros/criar.php
@@ -3,7 +3,22 @@
 ob_start();
 require('../../sheep_core/config.php');
 
+$produto = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+if(isset($produto['criarProduto'])){
+    unset($produto['criarProduto']);
+    $produto['capa'] = ($_FILES['capa']['tmp_name'] ? $_FILES['capa'] : null);
+    var_dump($produto);
 
+    $salvar = new Produtos();
+    $salvar -> criarProduto($produto);
+
+    if(salvar -> getResultato()){
+        header("Location: ".Home."/painel/index.php?sucesso=true");
+    }else{
+        header("Location: ".Home."/painel/index.php?erro=true");
+
+    }
+}
 
 
 ?>
--- sheep_core/gerentes/Produtos.php
+++ sheep_core/gerentes/Produtos.php
@@ -2,74 +2,49 @@
 
 class Produtos
 {
-
     private $Data;
     private $Resultado;
 
-    const BD = 'produtos';
+    const BD = 'produto';
 
-    public function CriarProduto(array $data)
-    {
-
+    public function criarProduto(array $data){
         $this->Data = $data;
 
-        if(in_array('', $this->Data)){
-
-                       $this->Resultado = false;
-          
+        if(in-array('', this->Data)){
+                $this->Resultafo = false;
         }else{
             if(isset($this->Data['capa'])){
                 $enviaFoto = new Uploads('../../uploads/');
-                $enviaFoto->Image($this->Data['capa'], date('Y-m-d').time());
-             }
-             if(isset($enviaFoto) && $enviaFoto->getResult()){
-                $this->Data['capa'] = $this->Data['capa'] != null ?  $enviaFoto->getResult() : null;
-                
-                    $this->Banco();
-                    $this->Criar();
-             }
+                $enviaFoto->image($this->Date['capa'], date('Y-m-d').time());
+
+            }
+            if(isset($enviaFoto)&& $enviaFoto->getResultado()){
+            $this->Data['capa'] = $this->Data['capa'] != null ? $enviaFoto : null;
+            
+            $this->Banco();
+            $this->Criar();
+            }
         }
-
     }
 
-    public function getResultado()
-    {
-        return $this->Resultado;
+    public function getResultado(){
+       return $this->Resultado;
     }
 
-
-    //private
-
-    private function Banco()
-    {
-
+    private function Banco(){
         $capa = $this->Data['capa'];
-
         unset($this->Data['capa']);
 
         $this->Data = array_map('addslashes', $this->Data);
-        $this->Data = array_map('htmlspecialchars', $this->Data);
+        $this->Data = array_map('htmlspacialchars', $this->Data);
         $this->Data = array_map('trim', $this->Data);
-        preg_replace('/[^[:alnum:]@]/', '',  $this->Data);
+        preg_replace('/[^[:alnum:]@]/','', $this->Data);
+
         $this->Data['capa'] = $capa;
         $this->Data['nome'] = (string) $this->Data['nome'];
-        $this->Data['valor'] = (string) $this->Data['valor'];
-        $this->Data['data'] = date('Y-m-d H:i:s');
-        
+        $this->Data['nome'] = (int) $this->Data['valor'];
+        $this->Data['nome'] = data('Y-m-d H:i:s');
     }
-
-
-    private function Criar()
-    {
-        $criar = new Criar();
-        $criar->Criacao(self::BD, $this->Data);
-        if(!$criar->getResultado()){
-            $this->Resultado = false;
-        }else{
-            $this->Resultado = true;
-        }
-    }
-
 }
 
 ?>
